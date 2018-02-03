06cf64b68cd963766780da314bbf2341df9b3621
gits2501 <gits2501@users.noreply.github.com>
2018-02-03 20:52:07 +0100

    Change in twiz-client(129)

--- scripts/twitting_main.js
+++ scripts/twitting_main.js
@@ -67,7 +67,7 @@
            var p = secondPhase.flow(options);
            if(p)p.then(function(o){
                   if(o.error) console.log('error (twitter)', o.error);
-                  if(o.data) conosole.log('success data: ', o.data)
+                  if(o.data) console.log('success data: ', o.data)
            }).catch(function(e){
                console.log('error in promise: ', e )
            })
