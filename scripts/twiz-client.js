873bfab4053eda35e1b6100ba28bba4cc0eff537
gits2501 <gits2501@users.noreply.github.com>
2018-02-05 13:33:01 +0100

    Change in twiz-client(140)

--- scripts/twiz-client.js
+++ scripts/twiz-client.js
@@ -714,7 +714,7 @@
    Redirect.prototype.deliverData = function(resolve, obj){ // delivers data to user by promise or
                                                                         // by callback function
      console.log('in deliverData, obj:', obj)
-      if(resolve){
+      if(resolve){ console.log('has promise')
           resolve(obj);
           return;
       }
